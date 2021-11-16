8e3b3cb16f6aa666d075d425d24360c56ae4b310
qinghuqu <qinghuqu@gmail.com>
2021-11-16 15:59:04 +0100

    Animated collapsible menu for mobile

--- js/script.js
+++ js/script.js
@@ -1,10 +1,15 @@
 var menuButton = document.querySelector("#menuButton");
 var navList = document.querySelector("#navList")
+var navMenu = document.querySelector("#navMenu")
 
 menuButton.addEventListener("click", () =>
 {
-  if (navList.style.display == "block")
+  if (getComputedStyle(navList).maxHeight == "0px")
   {
-    navList.style.display = "none";
+    navList.style.maxHeight = navList.scrollHeight + "px";
+  }
+  else if (getComputedStyle(navList).maxHeight == navList.scrollHeight + "px")
+  {
+    navList.style.maxHeight = "0px";
   }
 });
